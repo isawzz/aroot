0ae8440b7761195ff80d0b3a7352ede57523920e
isawzz <iwgajam@gmail.com>
2022-06-30 06:27:28 -0700

    das

--- easy/baustelle.js
+++ easy/baustelle.js
@@ -1,97 +1,33 @@
-function end_of_turn_fritz() {
+function ferro_is_set(cards, max_jollies_allowed = 1, seqlen = 7) {
+	//let [plorder, stage, A, fen, uplayer] = [Z.plorder, Z.stage, Z.A, Z.fen, Z.uplayer];
 
-	//#region prelim
-	let [A, fen, uplayer, plorder] = [Z.A, Z.fen, Z.uplayer, Z.plorder];
-	let pl = fen.players[uplayer];
-	//console.log('__________________________');
+	if (cards.length < 3) return false;
+	let num_jollies_in_cards = cards.filter(x => is_joker(x)).length;
+	if (num_jollies_in_cards > max_jollies_allowed) return false;
 
-	clear_quick_buttons();
-	let ms = fen.players[uplayer].time_left = stop_timer();
+	cards = sortCardItemsByRank(cards.map(x => x), rankstr = '23456789TJQKA*');
 
-	//#endregion
+	let rank = cards[0].rank;
+	if (cards.every(x => x.rank == rank || is_joker(x))) return cards.map(x => x.key);
 
-	//#region TJ group processing
+	let suit = cards[0].suit;
+	if (!cards.every(x => is_jolly(x.key) || x.suit == suit)) return false;
 
-	//all TJ groups must be checked and loose cards placed in loosecards
-	let ploose = {};
-	fen.journeys = [];
-	fen.loosecards = [];
-	for (const plname in fen.players) { fen.players[plname].loosecards = []; }
-	for (const group of DA.TJ) {
-		let ch = arrChildren(iDiv(group));
-		let cards = ch.map(x => Items[x.id]);
-		//find out if is a set
-		//console.log('cards', cards);
-		let set = ferro_is_set(cards, Z.options.jokers_per_group, 3);
-		//console.log('set', set);
-		if (!set) {
-			//dann kommen die Karten in die Loosecards
-			for (const card of cards) {
-				if (is_joker(card)) {
-					//console.log('pushing joker', card.key);
-					fen.loosecards.push(card.key);
-					continue;
-				}
-				let owner = valf(card.owner, uplayer);
-				lookupAddToList(ploose, [owner], card.key);
-				//console.log('add card', card.key, 'to', owner);
-			}
-			//console.log('NOT A SET', cards);
-		} else {
-			let j = set; //[];
-			//for (const card of cards) { delete card.owner; j.push(card.key); }
-			fen.journeys.push(j);
-			//console.log('YES!!!', 'adding journey', j);
-		}
-	}
-	for (const plname in ploose) {
-		fen.players[plname].loosecards = ploose[plname];
-	}
+	//if duplicate keys in cards, then it's not a set
+	let keys = cards.map(x => x.key);
+	if (keys.length != new Set(keys).size) return false;
 
-	//console.log('_____\npublic loosecards', fen.loosecards);
-	//console.log('journeys:', fen.journeys);
-	//for (const plname in fen.players) { console.log('loosecards', plname, fen.players[plname].loosecards); }
-
-	//discard pile must be reduced by all cards that do not have source = 'discard'
-	let discard = UI.deck_discard.items.filter(x => x.source == 'discard');
-	fen.deck_discard = discard.map(x => x.key);
-
-	if (!isEmpty(A.selected)) {
-		//console.log('selected', A.selected);
-		let ui_discarded_card = A.selected.map(x => A.items[x].o)[0];
-
-		removeInPlace(UI.players[uplayer].hand.items, ui_discarded_card);
-		ckey = ui_discarded_card.key;
-		//console.log('discard', discard);
-		elem_from_to(ckey, fen.players[uplayer].hand, fen.deck_discard);
-		//ari_history_list([`${uplayer} discards ${c}`], 'discard');
-
-	}
-
-	//all UI.hand cards that do NOT have source=hand must be removed from player hands
-	let uihand = UI.players[uplayer].hand.items; //.filter(x => x.source == 'hand');
-	let fenhand_vorher = fen.players[uplayer].hand;
-	let fenhand = fen.players[uplayer].hand = uihand.filter(x => x.source == 'hand').map(x => x.key);
-	//console.log('hand', uihand, 'fenhand vorher:', fenhand_vorher, 'fenhand', fenhand);
-
-	//#endregion
-
-	console.log('________ plorder', fen.plorder, 'turn', Z.turn);
-
-	//check round end
-	if (isEmpty(fenhand) && isEmpty(fen.players[uplayer].loosecards)) {
-		end_of_round_fritz(uplayer);
-	} else if (ms <= 100) {
-		console.log(`time is up for ${uplayer}!!!`);
-		ari_history_list([`${uplayer} runs out of time`], 'action');
-		if (fen.plorder.length <= 1) { end_of_round_fritz(uplayer); }
-		else { Z.turn = [get_next_player(Z, uplayer)]; deck_deal_safe_fritz(fen, Z.turn[0]); removeInPlace(fen.plorder, uplayer); }
-	} else { Z.turn = [get_next_player(Z, uplayer)]; deck_deal_safe_fritz(fen, Z.turn[0]); }
-
-	console.log('...plorder', fen.plorder, 'turn', Z.turn); output_scores();
-	turn_send_move_update();
-
+	//console.log('checking for sequence!!!!!!!!!!!!!!!!!!!!!')
+	let at_most_jollies = Math.min(num_jollies_in_cards, max_jollies_allowed);
+	let num_jolly = sortCardItemsToSequence(cards, rankstr = '23456789TJQKA', at_most_jollies);
+	//console.log('num_jolly', num_jolly);
+	let cond1 = num_jolly <= at_most_jollies; //this sequence does not need more jollies than it should
+	let cond2 = cards.length >= seqlen; //console.log('cond2', cond2);
+	//console.log('cards', cards);
+	if (cond1 && cond2) return cards.map(x => x.key); else return false;
 }
+
+
 function output_scores() {
 	let fen = Z.fen;
 	for (const plname in fen.players) {
--- easy/ferro.js
+++ easy/ferro.js
@@ -495,34 +495,6 @@ function ensure_buttons_visible_ferro() {
 	}
 
 }
-function ferro_is_set(cards, max_jollies_allowed = 1, seqlen = 7) {
-	//let [plorder, stage, A, fen, uplayer] = [Z.plorder, Z.stage, Z.A, Z.fen, Z.uplayer];
-
-	if (cards.length < 3) return false;
-	let num_jollies_in_cards = cards.filter(x => is_joker(x)).length;
-	if (num_jollies_in_cards > max_jollies_allowed) return false;
-
-	cards = sortCardItemsByRank(cards.map(x => x), rankstr = '23456789TJQKA*');
-
-	let rank = cards[0].rank;
-	if (cards.every(x => x.rank == rank || is_joker(x))) return cards.map(x => x.key);
-
-	let suit = cards[0].suit;
-	if (!cards.every(x => is_jolly(x.key) || x.suit == suit)) return false;
-
-	//if duplicate keys in cards, then it's not a set
-	let keys = cards.map(x => x.key);
-	if (keys.length != new Set(keys).size) return false;
-
-	//console.log('checking for sequence!!!!!!!!!!!!!!!!!!!!!')
-	let at_most_jollies = Math.min(num_jollies_in_cards, max_jollies_allowed);
-	let num_jolly = sortCardItemsToSequence(cards, rankstr = '23456789TJQKA', at_most_jollies);
-	//console.log('num_jolly', num_jolly);
-	let cond1 = num_jolly <= at_most_jollies; //this sequence does not need more jollies than it should
-	let cond2 = cards.length >= seqlen; //console.log('cond2', cond2);
-	//console.log('cards', cards);
-	if (cond1 && cond2) return cards.map(x => x.key); else return false;
-}
 function ferro_process_discard() {
 	let [plorder, stage, A, fen, uplayer] = [Z.plorder, Z.stage, Z.A, Z.fen, Z.uplayer];
 	let pl = fen.players[uplayer];
