08afc5c821644a3846364a21f58c3bc1d685552c
isawzz <iwgajam@gmail.com>
2023-03-26 16:36:17 -0700

    das

---
+++ SAFE/games_1/baustelle2.js
@@ -0,0 +1,36 @@
+
+function accuse_replaced_membership() {
+	let [stage, A, uplayer, fen, accused] = [Z.stage, Z.A, Z.uplayer, Z.fen, Z.fen.accused];
+
+	assertion(accused == uplayer, "accuse_replace_membership: WRONG PLAYER!!!!")
+	let card = A.items[A.selected[0]].a;
+	//remove from hand, set membership
+	let pl = fen.players[uplayer];
+	accuse_discard(pl.membership)
+	pl.membership = card;
+	removeInPlace(pl.hand, card);
+	ari_history_list(`${accused} chooses new membership` + (DA.showTestButtons ? ` ${card}` : ''), 'accuse');
+	delete fen.msg;
+	if (stage == 'accuse_action_entlarvt'){
+		Z.turn = [fen.president];
+		Z.stage = 'accuse_action_policy';
+		take_turn_fen_clear(); //!!!!clear added!!!!
+	}else{
+		fen.newpresident = accused;
+		set_new_president();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
