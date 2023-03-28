a58697fb76dcc4d1122286a4f91594a61ea471bb
isawzz <iwgajam@gmail.com>
2023-03-28 07:56:29 -0700

    das

--- hallo/start.js
+++
@@ -1,7 +0,0 @@
-onload = start; //document.onBlur = stopPolling;//onblur = stopPolling;//onfocus = onclick_reload_after_switching;
-//DA.SIMSIM = true; //DA.exclusive = true; DA.TESTSTART1 = true; //DA.sendmax = 3; //DA.TEST0 = true; //DA.TEST1 = true; DA.TEST1Counter = 0;
-
-
-
-
-
